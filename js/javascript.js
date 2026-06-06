fa6ce6ed13d3b221c839f738910215ca79d58ec1
Lucas Mazzeo <lucasmazzeo221@gmail.com>
2026-06-06 14:33:48 -0300

    Criando o carrossel de imagens.

--- js/javascript.js
+++ js/javascript.js
@@ -10,3 +10,49 @@ botaoTema.onclick = function () {
         botaoTema.textContent = "Modo Claro";
     }
 };
+
+const carouselSlides = document.querySelectorAll('.carousel-slide');
+const prevBtn = document.querySelector('.carousel-btn.prev');
+const nextBtn = document.querySelector('.carousel-btn.next');
+let currentSlide = 0;
+let carouselInterval;
+
+function showSlide(index) {
+    carouselSlides.forEach(slide => slide.classList.remove('active'));
+    carouselSlides[index].classList.add('active');
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % carouselSlides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + carouselSlides.length) % carouselSlides.length;
+    showSlide(currentSlide);
+}
+
+function startAutoCarousel() {
+    carouselInterval = setInterval(nextSlide, 5000);
+}
+
+function stopAutoCarousel() {
+    clearInterval(carouselInterval);
+}
+
+if (carouselSlides.length > 0) {
+    showSlide(0);
+    startAutoCarousel();
+
+    prevBtn?.addEventListener('click', () => {
+        prevSlide();
+        stopAutoCarousel();
+        startAutoCarousel();
+    });
+
+    nextBtn?.addEventListener('click', () => {
+        nextSlide();
+        stopAutoCarousel();
+        startAutoCarousel();
+    });
+}
