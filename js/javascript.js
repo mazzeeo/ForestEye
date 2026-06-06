1b02fd5d6dc577d0f5b6bfaabb2d17080690fa91
Lucas Mazzeo <lucasmazzeo221@gmail.com>
2026-06-06 14:49:53 -0300

    Carrossel Criado.

--- js/javascript.js
+++ js/javascript.js
@@ -11,48 +11,16 @@ botaoTema.onclick = function () {
     }
 };
 
-const carouselSlides = document.querySelectorAll('.carousel-slide');
-const prevBtn = document.querySelector('.carousel-btn.prev');
-const nextBtn = document.querySelector('.carousel-btn.next');
-let currentSlide = 0;
-let carouselInterval;
-
-function showSlide(index) {
-    carouselSlides.forEach(slide => slide.classList.remove('active'));
-    carouselSlides[index].classList.add('active');
-}
-
-function nextSlide() {
-    currentSlide = (currentSlide + 1) % carouselSlides.length;
-    showSlide(currentSlide);
-}
-
-function prevSlide() {
-    currentSlide = (currentSlide - 1 + carouselSlides.length) % carouselSlides.length;
-    showSlide(currentSlide);
-}
-
-function startAutoCarousel() {
-    carouselInterval = setInterval(nextSlide, 5000);
-}
-
-function stopAutoCarousel() {
-    clearInterval(carouselInterval);
-}
-
-if (carouselSlides.length > 0) {
-    showSlide(0);
-    startAutoCarousel();
-
-    prevBtn?.addEventListener('click', () => {
-        prevSlide();
-        stopAutoCarousel();
-        startAutoCarousel();
-    });
-
-    nextBtn?.addEventListener('click', () => {
-        nextSlide();
-        stopAutoCarousel();
-        startAutoCarousel();
-    });
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelectorAll(".carousel-slide");
+    let currentSlideIndex = 0;
+    const slideIntervalTime = 5000; 
+    function nextSlide() {
+        slides[currentSlideIndex].classList.remove("active");
+        currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+        slides[currentSlideIndex].classList.add("active");
+    }
+    if (slides.length > 1) {
+        setInterval(nextSlide, slideIntervalTime);
+    }
+});
